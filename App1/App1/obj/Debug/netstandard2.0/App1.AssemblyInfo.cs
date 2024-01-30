2fddcba88460a654f4fb799edee021cdef55f0ce
BrayanPCH <bapulamarinc@utn.edu.ec>
2024-01-30 12:09:57 -0500

    Assign credentials

--- App1/App1/obj/Debug/netstandard2.0/App1.AssemblyInfo.cs
+++ App1/App1/obj/Debug/netstandard2.0/App1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("App1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d11baa2a19ba0e63f6c09dd29bde7b29d983a574")]
 [assembly: System.Reflection.AssemblyProductAttribute("App1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("App1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
